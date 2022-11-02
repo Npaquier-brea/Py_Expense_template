50db88b4d480dc3ca5f7212b0585c7494e96b56d
nicolas.paquier-brea <nicolas.paquier-brea@epita.fr>
2022-11-02 22:18:18 +0100

    8 step

--- expense.py
+++ expense.py
@@ -7,13 +7,10 @@ users_checkbox = []
 
 with open('users.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
-    print(csv_reader)
     for row in csv_reader:
         users.append(row[0])
         users_checkbox.append({'name': row[0]})
         
-print(users)
-
 expense_questions = [
     {
         "type":"input",
@@ -29,25 +26,43 @@ expense_questions = [
         "type":"list",
         "name":"spender",
         "message":"New Expense - Spender: ",
-        'choices':users
+        'choices':users,
     },
     {
         "type":"checkbox",
         "name":"users_involved",
         "message":"New Expense - Users Involved: ",
-        "choices":users_checkbox
+        "choices":users_checkbox,
     },
 ]
 
+def calculate_balance(amount, nb_users, users, spender):
+    balance = []
+    amount_spender = amount / nb_users * (nb_users - 1)
+    amount_users_involved =  0 - amount / nb_users
+    balance.append({'name': spender,'balance': str(amount_spender)})
+    for user in users:
+        balance.append({'name': user,'balance': str(amount_users_involved), 'spender': spender})
+    return balance
+    
 def new_expense(*args):
     infos = prompt(expense_questions)
-    filename = "expense_report.csv"
-    with open(filename, 'a') as csvfile:
+    filename1 = "expense_report.csv"
+    filename2 = "balance.csv"
+    with open(filename1, 'a') as csvfile:
         tmp_data = [infos['amount'], infos['label'], infos['spender']]
         csvwriter = csv.writer(csvfile)
         for user_involved in infos['users_involved']:
             tmp_data.append(user_involved)
         csvwriter.writerow(tmp_data)
+        csvfile.close()
+    with open(filename2, 'a') as csvfile:
+        #infos['users_involved'].remove(infos['spender'])
+        csvwriter = csv.writer(csvfile)
+        balance = calculate_balance(int(infos['amount']), len(infos['users_involved']), infos['users_involved'], infos['spender'])
+        for elm in balance:
+            csvwriter.writerow([elm['name'], elm['balance']])
+        csvfile.close()
     print("Expense Added !")
     return True
 
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PyInquirer import prompt
 from examples import custom_style_2
 from expense import expense_questions,new_expense
 from user import add_user
+import csv
 
 def ask_option():
     main_option = {
@@ -16,6 +17,18 @@ def ask_option():
         ask_option()
     if (option['main_options']) == "New User":
         add_user()
+        ask_option()
+    if (option['main_options']) == "Show Status":
+        with open('balance.csv') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                if len(row) >= 3:
+                    if float(row[1]) >= 0:
+                        print(row[0] + " owes nothing")
+                    else:
+                        print(row[0] + " owes " + str(round(-float(row[1]),2)) + "€ to " + row[2])
+                else:
+                    print(row[0] + " owes nothing")
         ask_option()
 
 def main():
